afd127bb9443142c72e136b5e332ffe1b163c0c3
Balázs Ruda <balazs.ruda@supercharge.io>
2022-12-31 22:21:28 +0100

    fix: add Vendor in order to prevent build warning in projects

--- lint-checks-android-rules/src/main/kotlin/com/balazsruda/lintchecks/android/IssueRegistry.kt
+++ lint-checks-android-rules/src/main/kotlin/com/balazsruda/lintchecks/android/IssueRegistry.kt
@@ -1,5 +1,6 @@
 package com.balazsruda.lintchecks.android
 
+import com.android.tools.lint.client.api.Vendor
 import com.android.tools.lint.detector.api.CURRENT_API
 
 internal const val DEFAULT_PRIORITY = 10 // Can be ignored
@@ -10,5 +11,13 @@ class IssueRegistry : com.android.tools.lint.client.api.IssueRegistry() {
     override val issues
         get() = listOf(
             ISSUE_CONSTRAINT_LAYOUT_MATCH_PARENT_ATTRIBUTE_DETECTOR
+        )
+
+    override val vendor: Vendor
+        get() = Vendor(
+            vendorName = "Team Supercharge",
+            identifier = "lint-checks",
+            feedbackUrl = "https://github.com/team-supercharge/lint-checks/issues",
+            contact = "https://github.com/team-supercharge/lint-checks"
         )
 }
